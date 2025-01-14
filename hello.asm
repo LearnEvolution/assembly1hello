c533144f5d14b21b906ea88c6535447771654ecd
evolution <psh.evolution@gmail.com>
2025-01-14 01:02:22 -0300

    assembly2

--- hello.asm
+++ hello.asm
@@ -1,11 +1,18 @@
 section .data
+    msg db 'hello world'
+    tam equ $- msg
 
 section .text
 
 global _start
 
 _start: 
-        
-    mov eax. 0x1 
-    mov exa. 0x0
+    mov eax. 0x4     
+    mov ebx. 0x1 
+    mov ecx. msg
+    mov edx. tam 
+    int 0x80
+
+    mov eax. 0x1
+    mov ebx. 0x0
     int 0x80
